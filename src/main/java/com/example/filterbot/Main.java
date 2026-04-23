380f42ab26edacb5a3133af3e4d1e8ecb6ec4463
Oleksii <Oleksij.Gryszko@gmail.com>
2026-04-23 13:56:40 +0200

    Update Main.java

--- src/main/java/com/example/filterbot/Main.java
+++ src/main/java/com/example/filterbot/Main.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // <-- THIS IS THE MAGIC SWITCH FOR THE TIMER
+@EnableScheduling 
 public class Main {
 
     public static void main(String[] args) {
-        // This single line starts the whole Spring Boot framework.
-        // It automatically finds your IswTime class, creates it, and starts the 60-second timer in the background.
+        
         SpringApplication.run(Main.class, args);
     }
 
